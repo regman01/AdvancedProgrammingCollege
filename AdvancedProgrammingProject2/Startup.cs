104b0787fa52631619ebca867b4ac79b9520734e
p2598200 <p2598200@GWHXX683WIN005W>
2022-03-22 14:10:31 +0000

    Add project files.

---
+++ AdvancedProgrammingProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdvancedProgrammingProject2.Startup))]
+namespace AdvancedProgrammingProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
